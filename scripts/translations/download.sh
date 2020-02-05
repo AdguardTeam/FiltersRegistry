ba6b4d634d155bb40d5f784f3d466f1c2afa1a25
tvinzz <15062885+tvinzz@users.noreply.github.com>
2020-02-06 00:24:09 +0700

    Updated locales

--- scripts/translations/download.sh
+++ scripts/translations/download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SERVICE_URL="https://twosky.adtidy.org/api/v1/"
 workDir=../..
-locales=("en" "ru" "ar" "bg" "ca" "zh" "zh_TW" "hr" "da" "nl" "fi" "fr" "de" "he" "hu" "id" "it" "ja" "ko" "no" "fa" "pl" "pt" "pt_BR" "pt_PT" "sr_Latn" "sk" "es_ES" "sv" "tr" "uk" "vi" "be_BY" "sl_SI")
+locales=("en" "ru" "ar" "bg" "ca" "zh_CN" "zh_TW" "hr" "da" "nl" "fi" "fr" "de" "he" "hu" "id" "it" "ja" "ko" "no" "fa" "pl" "pt" "pt_BR" "pt_PT" "sr" "sk" "es" "sv" "tr" "uk" "vi" "be" "sl")
 
 for locale in "${locales[@]}"
 do
@@ -9,17 +9,9 @@ do
     curl "${SERVICE_URL}download?format=strings&language=${locale}&filename=tags.json&project=filters-registry" -o messages.json
 
     destinationLocale=$locale
-    if [ "$locale" = "sr_Latn" ]; then
-        echo "Change $locale destination dir to sr"
-        destinationLocale=sr
-    fi
-    if [ "$locale" = "be_BY" ]; then
-        echo "Change $locale destination dir to be"
-        destinationLocale=be
-    fi
-    if [ "$locale" = "sl_SI" ]; then
-        echo "Change $locale destination dir to sl"
-        destinationLocale=sl
+    if [ "$locale" = "zh_CN" ]; then
+        echo "Change $locale destination dir to zh"
+        destinationLocale=zh
     fi
 
     mkdir -p $workDir/locales/$destinationLocale
@@ -30,14 +22,14 @@ do
     echo "Moving tags.json for $locale locale to $workDir/locales/$destinationLocale/"
     cp -f converted.json $workDir/locales/$destinationLocale/tags.json
 
-    if [ "$locale" = "es_ES" ]; then
-        echo "Copying tags.json for es locale"
-        cp -f converted.json $workDir/locales/es/tags.json
+    if [ "$locale" = "es" ]; then
+        echo "Copying tags.json for es_ES locale"
+        cp -f converted.json $workDir/locales/es_ES/tags.json
     fi
 
-    if [ "$locale" = "pt_PT" ]; then
-        echo "Copying tags.json for pt locale"
-        cp -f converted.json $workDir/locales/pt/tags.json
+    if [ "$locale" = "pt" ]; then
+        echo "Copying tags.json for pt_PT locale"
+        cp -f converted.json $workDir/locales/pt_PT/tags.json
     fi
 
     rm messages.json
@@ -50,17 +42,9 @@ do
     curl "${SERVICE_URL}download?format=strings&language=${locale}&filename=groups.json&project=filters-registry" -o messages.json
 
     destinationLocale=$locale
-    if [ "$locale" = "sr-Latn" ]; then
-        echo "Change $locale destination dir to sr"
-        destinationLocale=sr
-    fi
-    if [ "$locale" = "be_BY" ]; then
-        echo "Change $locale destination dir to be"
-        destinationLocale=be
-    fi
-    if [ "$locale" = "sl_SI" ]; then
-        echo "Change $locale destination dir to sl"
-        destinationLocale=sl
+    if [ "$locale" = "zh_CN" ]; then
+        echo "Change $locale destination dir to zh"
+        destinationLocale=zh
     fi
 
     echo "Parsing groups.json for $locale locale for groups.json"
@@ -69,14 +53,14 @@ do
     echo "Moving groups.json for $locale locale to $workDir/locales/$destinationLocale/"
     cp -f converted.json $workDir/locales/$destinationLocale/groups.json
 
-    if [ "$locale" = "es_ES" ]; then
-        echo "Copying groups.json for es locale"
-        cp -f converted.json $workDir/locales/es/groups.json
+    if [ "$locale" = "es" ]; then
+        echo "Copying groups.json for es_ES locale"
+        cp -f converted.json $workDir/locales/es_ES/groups.json
     fi
 
-    if [ "$locale" = "pt_PT" ]; then
-        echo "Copying groups.json for pt locale"
-        cp -f converted.json $workDir/locales/pt/groups.json
+    if [ "$locale" = "pt" ]; then
+        echo "Copying groups.json for pt_PT locale"
+        cp -f converted.json $workDir/locales/pt_PT/groups.json
     fi
 
     rm messages.json
@@ -89,17 +73,9 @@ do
     curl "${SERVICE_URL}download?format=strings&language=${locale}&filename=filters.json&project=filters-registry" -o messages.json
 
     destinationLocale=$locale
-    if [ "$locale" = "sr-Latn" ]; then
-        echo "Change $locale destination dir to sr"
-        destinationLocale=sr
-    fi
-    if [ "$locale" = "be_BY" ]; then
-        echo "Change $locale destination dir to be"
-        destinationLocale=be
-    fi
-    if [ "$locale" = "sl_SI" ]; then
-        echo "Change $locale destination dir to sl"
-        destinationLocale=sl
+    if [ "$locale" = "zh_CN" ]; then
+        echo "Change $locale destination dir to zh"
+        destinationLocale=zh
     fi
 
     echo "Parsing filters.json for $locale locale for filters.json"
@@ -108,13 +84,13 @@ do
     echo "Moving filters.json for $locale locale to $workDir/locales/$destinationLocale/"
     cp -f converted.json $workDir/locales/$destinationLocale/filters.json
 
-    if [ "$locale" = "es_ES" ]; then
+    if [ "$locale" = "es" ]; then
         echo "Copying filters.json for es locale"
-        cp -f converted.json $workDir/locales/es/filters.json
+        cp -f converted.json $workDir/locales/es_ES/filters.json
     fi
 
-    if [ "$locale" = "pt_PT" ]; then
-        echo "Copying filters.json for pt locale"
+    if [ "$locale" = "pt" ]; then
+        echo "Copying filters.json for pt_PT locale"
         cp -f converted.json $workDir/locales/pt/filters.json
     fi
 
