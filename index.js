54d54980fdb9ffe5c1991b73afc5d7c9cde26aa3
Andrey Meshkov <am@adguard.com>
2020-06-09 00:07:47 +0300

    fix: 🐛 yarn lock

--- index.js
+++ index.js
@@ -19,9 +19,14 @@ const compiler = require("adguard-filters-compiler");
 
 const filtersDir = path.join(__dirname, './filters');
 const logPath = path.join(__dirname, './log.txt');
-const reportPath = path.join(__dirname, './report.txt');
-const domainBlacklistFile = path.join(__dirname, './domains-blacklist.txt');
 
+let reportPath = path.join(__dirname, './report.txt');
+if (whitelist.length > 0 || blacklist.length > 0) {
+    // Disable report if this is not a full build
+    reportPath = null;
+}
+
+const domainBlacklistFile = path.join(__dirname, './domains-blacklist.txt');
 const platformsPath = path.join(__dirname, './platforms');
 
 compiler.compile(filtersDir, logPath, reportPath, domainBlacklistFile, platformsPath, whitelist, blacklist);
