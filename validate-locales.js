ad13b489fc452542769cbc22c3eb6cd2917a6d40
Slava Leleka <v.leleka@adguard.com>
2020-11-19 16:21:26 +0300

    add required locales

--- validate-locales.js
+++ validate-locales.js
@@ -4,8 +4,22 @@ const compiler = require("adguard-filters-compiler");
 
 const LOCALES_DIR_PATH = './locales';
 
+const REQUIRED_LOCALES = [
+    // kepp base locale here as well
+    'en',
+    // + other our locales
+    'ru',
+    'de',
+    'fr',
+    'it',
+    'ja',
+    'ko',
+    'zh_CN',
+];
+
 const localesDirPath = path.join(__dirname, LOCALES_DIR_PATH);
-const localesValidationResult = compiler.validateLocales(localesDirPath);
-if (localesValidationResult.length !== 0) {
+const localesValidation = compiler.validateLocales(localesDirPath, REQUIRED_LOCALES);
+
+if (!localesValidation.ok) {
     throw new Error('Invalid locales messages');
 }
