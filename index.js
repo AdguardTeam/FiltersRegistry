43736e80a0202417ae7bf01fb9d71d4d53f5c3c8
zloyden <v.marchishin@adguard.com>
2022-08-29 11:16:06 +0300

    Feature https://github.com/AdguardTeam/FiltersRegistry/issues/662

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ args.forEach(function (val) {
     }
 });
 
+const fs = require('fs');
 const path = require('path');
 const compiler = require("adguard-filters-compiler");
 
@@ -29,4 +30,57 @@ if (whitelist.length > 0 || blacklist.length > 0) {
 const customPlatformsConfig = require('./custom_platforms.js');
 const platformsPath = path.join(__dirname, './platforms');
 
-compiler.compile(filtersDir, logPath, reportPath, platformsPath, whitelist, blacklist, customPlatformsConfig);
+/**
+ * Search for specific files by extension type and
+ * replaces all occurrencies of necessary strings to new ones in the build results.
+ */
+
+ async function replaceInFilters() {
+/**
+ *  
+ * @param {String} dir - specific directory that limits the search.
+ * @param {String} type - type of the file.
+ */
+    function searchFiles(dir, type) {
+        let values = [];
+        let subDirs = fs.readdirSync(dir);
+        subDirs.forEach((subDir) => {
+            subDir = path.resolve(dir, subDir);
+            let statElement = fs.statSync(subDir);
+            if (statElement.isDirectory() && path.basename(subDir) !== 'android-content-blocker') {
+                values = values.concat(searchFiles(subDir, type));
+            }
+            if (statElement.isFile()
+                && !subDir.includes('local_script_rules')
+                && path.extname(subDir) === type) {
+                values.push(subDir);
+            }
+          });
+        return values;
+      };
+      let filesTxt = searchFiles(platformsPath, '.txt');
+      filesTxt.forEach((fileTxt) => {
+        let data = fs.readFileSync(fileTxt, 'utf8');
+        let formattedData = data.replaceAll(/:has\(/g, ':-abp-has(');
+        fs.writeFileSync(fileTxt, formattedData, 'utf8');
+      });
+}
+
+/**
+ * Compiler entry point.
+ */
+async function main() {
+    await compiler.compile(
+        filtersDir,
+        logPath,
+        reportPath,
+        platformsPath,
+        whitelist,
+        blacklist,
+        customPlatformsConfig,
+    );
+    // TODO: remove this when ExtCSS v2.0 is released to all AG products.
+    await replaceInFilters();
+}
+
+main();
