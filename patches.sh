14601066e5716cf26535453de6c49d370e4c9289
Dmitriy Seregin <d.seregin@adguard.com>
2023-11-29 20:43:54 +0300

    fixes

--- patches.sh
+++ patches.sh
@@ -11,7 +11,8 @@ DIFF_BUILDER=node_modules/@adguard/diff-builder/dist/diff-builder
 chmod +x "$DIFF_BUILDER"
 
 # Iterate over all *.txt files in all 'filters/' folders inside $FOLDER_WITH_NEW_FILTERS
-all_filters=$(find $FOLDER_WITH_NEW_FILTERS/**/filters -type f -name "*.txt")
+all_filters=$(find platforms -type d -name filters -exec find {} -type f -name "*.txt" \;)
+echo $all_filters
 for new_filter in $all_filters; do
     # Check if file exists
     if [ -e "$new_filter" ]; then
@@ -25,6 +26,9 @@ for new_filter in $all_filters; do
         path_to_patches="$parent_dir/patches/$basename"
 
         # Generate patches
-        $($DIFF_BUILDER build $old_filter $new_filter $path_to_patches -n $basename -r h -t 1)
+        $DIFF_BUILDER build -n $basename -r h -t 1 -v $old_filter $new_filter $path_to_patches
     fi
 done
+
+# Move all generated patches with fresh filters into old_platforms
+cp -a platforms/. old_platforms/
