9ca3fefa856ee0516da406a81d0c338efe692d9e
Filters Builder <filters@adguard.com>
2023-12-27 23:09:17 +0300

    fixes

--- scripts/repository/compress.js
+++ scripts/repository/compress.js
@@ -66,7 +66,7 @@ async function squashAndPush() {
     // Step 7: Cherry-pick the commits you want to store
     // Use the `log` method with a range specification to get the commit history
     const historyToSave = await git.log({
-        from: `master~${commitsToKeep - 3}`,
+        from: `${squashedCommitHash}`,
         to: 'master',
         '--no-merges': true,
     });
