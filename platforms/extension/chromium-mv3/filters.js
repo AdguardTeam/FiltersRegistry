54cfa334b60600fa53ca05bd62135167cf12a75a
Filters Builder <filters@adguard.com>
2024-10-30 20:36:59 +0000

    skip ci. build from Wed Oct 30 20:36:59 UTC 2024

--- platforms/extension/chromium-mv3/filters.js
+++ platforms/extension/chromium-mv3/filters.js
@@ -359,8 +359,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/chromium-mv3/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.58.39",
-			"timeUpdated": "2024-10-30T19:31:27+0000",
+			"version": "2.3.58.40",
+			"timeUpdated": "2024-10-30T20:31:21+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
