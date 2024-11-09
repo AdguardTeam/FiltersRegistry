00b9124779551030a307b36e9e1a045fac45bdcd
Filters Builder <filters@adguard.com>
2024-11-09 06:37:44 +0000

    skip ci. build from Sat Nov  9 06:37:44 UTC 2024

--- platforms/extension/opera/filters.js
+++ platforms/extension/opera/filters.js
@@ -639,8 +639,8 @@
 				"ext_chromium_mv3"
 			],
 			"trustLevel": "full",
-			"version": "2.0.72.54",
-			"timeUpdated": "2024-11-09T00:33:29+0000",
+			"version": "2.0.72.55",
+			"timeUpdated": "2024-11-09T06:33:28+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
--- platforms/mac/filters.json
+++ platforms/mac/filters.json
@@ -250,8 +250,8 @@
 			"platformsExcluded": [
 				"ext_chromium_mv3"
 			],
-			"version": "2.0.72.54",
-			"timeUpdated": "2024-11-09T00:33:29+0000",
+			"version": "2.0.72.55",
+			"timeUpdated": "2024-11-09T06:33:28+0000",
 			"languages": []
 		},
 		{
