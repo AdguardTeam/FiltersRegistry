e31ed90d58268080f2e7cd61457dbfbdd0106748
Filters Builder <filters@adguard.com>
2024-11-10 15:37:56 +0000

    skip ci. build from Sun Nov 10 15:37:56 UTC 2024

--- platforms/extension/firefox/filters.js
+++ platforms/extension/firefox/filters.js
@@ -359,8 +359,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/firefox/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.59.99",
-			"timeUpdated": "2024-11-10T14:31:28+0000",
+			"version": "2.3.60.0",
+			"timeUpdated": "2024-11-10T15:31:31+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
@@ -382,8 +382,8 @@
 			"groupId": 2,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/firefox/filters/3.txt",
 			"trustLevel": "full",
-			"version": "2.0.76.39",
-			"timeUpdated": "2024-11-08T13:33:26+0000",
+			"version": "2.0.76.40",
+			"timeUpdated": "2024-11-10T15:33:04+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.59.99",
-			"timeUpdated": "2024-11-10T14:31:28+0000",
+			"version": "2.3.60.0",
+			"timeUpdated": "2024-11-10T15:31:31+0000",
 			"languages": []
 		},
 		{
@@ -74,8 +74,8 @@
 			"displayNumber": 1,
 			"groupId": 2,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/3.txt",
-			"version": "2.0.76.39",
-			"timeUpdated": "2024-11-08T13:33:26+0000",
+			"version": "2.0.76.40",
+			"timeUpdated": "2024-11-10T15:33:04+0000",
 			"languages": []
 		},
 		{
