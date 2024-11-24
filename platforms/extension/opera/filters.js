a2447dd4173f86a88ba9b5f15599b0c8b33c43ac
Filters Builder <filters@adguard.com>
2024-11-24 15:38:39 +0000

    skip ci. build from Sun Nov 24 15:38:39 UTC 2024

--- platforms/extension/opera/filters.js
+++ platforms/extension/opera/filters.js
@@ -1682,8 +1682,8 @@
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/opera/filters/224.txt",
 			"trustLevel": "full",
-			"version": "2.1.26.53",
-			"timeUpdated": "2024-11-24T11:34:29+0000",
+			"version": "2.1.26.54",
+			"timeUpdated": "2024-11-24T15:34:31+0000",
 			"deprecated": false,
 			"languages": [
 				"zh"
--- platforms/extension/safari/filters.js
+++ platforms/extension/safari/filters.js
@@ -1657,8 +1657,8 @@
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/safari/filters/224_optimized.txt",
 			"trustLevel": "full",
-			"version": "2.1.26.53",
-			"timeUpdated": "2024-11-24T11:34:29+0000",
+			"version": "2.1.26.54",
+			"timeUpdated": "2024-11-24T15:34:31+0000",
 			"deprecated": false,
 			"languages": [
 				"zh"
