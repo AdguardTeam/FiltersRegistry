1c9069b6ae3722441f2cd100c4969bd8c652f76f
Filters Builder <filters@adguard.com>
2024-11-27 02:38:30 +0000

    skip ci. build from Wed Nov 27 02:38:30 UTC 2024

--- platforms/extension/ublock/filters.js
+++ platforms/extension/ublock/filters.js
@@ -359,8 +359,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/ublock/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.62.22",
-			"timeUpdated": "2024-11-27T01:31:25+0000",
+			"version": "2.3.62.23",
+			"timeUpdated": "2024-11-27T02:31:24+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
