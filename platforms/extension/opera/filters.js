ccc6b3bb041fe7bfa2adccad60f74c652826bf05
Filters Builder <filters@adguard.com>
2024-11-27 22:38:07 +0000

    skip ci. build from Wed Nov 27 22:38:07 UTC 2024

--- platforms/extension/opera/filters.js
+++ platforms/extension/opera/filters.js
@@ -359,8 +359,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/opera/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.62.35",
-			"timeUpdated": "2024-11-27T18:31:27+0000",
+			"version": "2.3.62.36",
+			"timeUpdated": "2024-11-27T22:31:24+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
