20861ca0725c9568b8c58c46e61857be40ee9dca
Filters Builder <filters@adguard.com>
2024-12-01 03:38:30 +0000

    skip ci. build from Sun Dec  1 03:38:30 UTC 2024

--- platforms/extension/firefox/filters.js
+++ platforms/extension/firefox/filters.js
@@ -1682,8 +1682,8 @@
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/firefox/filters/224.txt",
 			"trustLevel": "full",
-			"version": "2.1.26.89",
-			"timeUpdated": "2024-12-01T02:35:01+0000",
+			"version": "2.1.26.90",
+			"timeUpdated": "2024-12-01T03:34:48+0000",
 			"deprecated": false,
 			"languages": [
 				"zh"
--- platforms/mac/filters.json
+++ platforms/mac/filters.json
@@ -1120,8 +1120,8 @@
 			"displayNumber": 1,
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/224.txt",
-			"version": "2.1.26.89",
-			"timeUpdated": "2024-12-01T02:35:01+0000",
+			"version": "2.1.26.90",
+			"timeUpdated": "2024-12-01T03:34:48+0000",
 			"languages": [
 				"zh"
 			]
