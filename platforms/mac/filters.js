d74977c0c213f667976840647708d1363532e5b2
Filters Builder <filters@adguard.com>
2024-12-27 17:39:38 +0000

    skip ci. build from Fri Dec 27 17:39:38 UTC 2024

--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -46,8 +46,8 @@
 			"displayNumber": 1,
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/1.txt",
-			"version": "2.0.96.37",
-			"timeUpdated": "2024-12-27T15:31:07+0000",
+			"version": "2.0.96.38",
+			"timeUpdated": "2024-12-27T17:31:10+0000",
 			"languages": [
 				"ru"
 			]
--- platforms/mac_v2/filters.js
+++ platforms/mac_v2/filters.js
@@ -334,8 +334,8 @@
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/mac_v2/filters/1.txt",
 			"trustLevel": "full",
-			"version": "2.0.96.37",
-			"timeUpdated": "2024-12-27T15:31:07+0000",
+			"version": "2.0.96.38",
+			"timeUpdated": "2024-12-27T17:31:10+0000",
 			"deprecated": false,
 			"languages": [
 				"ru"
