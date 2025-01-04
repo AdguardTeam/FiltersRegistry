4f70c5c477dad13702b91df2a9142c353a471bd3
Filters Builder <filters@adguard.com>
2025-01-04 05:43:48 +0000

    skip ci. build from Sat Jan  4 05:43:48 UTC 2025

--- platforms/extension/opera/filters.js
+++ platforms/extension/opera/filters.js
@@ -1682,8 +1682,8 @@
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/opera/filters/224.txt",
 			"trustLevel": "full",
-			"version": "2.1.29.23",
-			"timeUpdated": "2025-01-04T03:40:10+0000",
+			"version": "2.1.29.24",
+			"timeUpdated": "2025-01-04T05:40:04+0000",
 			"deprecated": false,
 			"languages": [
 				"zh"
--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -1120,8 +1120,8 @@
 			"displayNumber": 1,
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/224.txt",
-			"version": "2.1.29.23",
-			"timeUpdated": "2025-01-04T03:40:10+0000",
+			"version": "2.1.29.24",
+			"timeUpdated": "2025-01-04T05:40:04+0000",
 			"languages": [
 				"zh"
 			]
