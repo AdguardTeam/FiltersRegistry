44062f4de8f7330932d59c67ff4a11daf202084c
Filters Builder <filters@adguard.com>
2025-01-05 18:44:26 +0000

    skip ci. build from Sun Jan  5 18:44:26 UTC 2025

--- platforms/android/filters.js
+++ platforms/android/filters.js
@@ -639,8 +639,8 @@
 				"ext_chromium_mv3"
 			],
 			"trustLevel": "full",
-			"version": "2.0.74.92",
-			"timeUpdated": "2025-01-05T12:40:19+0000",
+			"version": "2.0.74.93",
+			"timeUpdated": "2025-01-05T18:40:24+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
