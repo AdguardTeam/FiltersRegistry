326033884adc0977119ef3d98fccd339a255c976
Filters Builder <filters@adguard.com>
2025-01-05 16:44:27 +0000

    skip ci. build from Sun Jan  5 16:44:26 UTC 2025

--- platforms/extension/chromium/filters.js
+++ platforms/extension/chromium/filters.js
@@ -359,8 +359,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/chromium/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.66.81",
-			"timeUpdated": "2025-01-05T14:31:26+0000",
+			"version": "2.3.66.82",
+			"timeUpdated": "2025-01-05T16:31:26+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
--- platforms/mac/filters.json
+++ platforms/mac/filters.json
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.66.81",
-			"timeUpdated": "2025-01-05T14:31:26+0000",
+			"version": "2.3.66.82",
+			"timeUpdated": "2025-01-05T16:31:26+0000",
 			"languages": []
 		},
 		{
