652097b954630c0d069c2ce7afcb4149aaca8a2c
Filters Builder <filters@adguard.com>
2025-01-18 22:43:05 +0000

    skip ci. build from Sat Jan 18 22:43:05 UTC 2025

--- platforms/android/filters.js
+++ platforms/android/filters.js
@@ -670,8 +670,8 @@
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/android/filters/16_optimized.txt",
 			"trustLevel": "full",
-			"version": "2.0.49.43",
-			"timeUpdated": "2025-01-18T20:38:52+0000",
+			"version": "2.0.49.44",
+			"timeUpdated": "2025-01-18T22:38:53+0000",
 			"deprecated": false,
 			"languages": [
 				"fr"
--- platforms/mac/filters.json
+++ platforms/mac/filters.json
@@ -263,8 +263,8 @@
 			"displayNumber": 1,
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/16.txt",
-			"version": "2.0.49.43",
-			"timeUpdated": "2025-01-18T20:38:52+0000",
+			"version": "2.0.49.44",
+			"timeUpdated": "2025-01-18T22:38:53+0000",
 			"languages": [
 				"fr"
 			]
