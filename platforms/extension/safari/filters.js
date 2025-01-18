706f870412cea8fba7fa2294d83f57bf31685156
Filters Builder <filters@adguard.com>
2025-01-18 08:43:33 +0000

    skip ci. build from Sat Jan 18 08:43:33 UTC 2025

--- platforms/extension/safari/filters.js
+++ platforms/extension/safari/filters.js
@@ -359,8 +359,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/safari/filters/2_optimized.txt",
 			"trustLevel": "full",
-			"version": "2.3.68.38",
-			"timeUpdated": "2025-01-18T07:31:35+0000",
+			"version": "2.3.68.39",
+			"timeUpdated": "2025-01-18T08:31:33+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
