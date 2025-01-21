a4d20431742a983b3a7233614b173589437da43d
Filters Builder <filters@adguard.com>
2025-01-21 22:46:03 +0000

    skip ci. build from Tue Jan 21 22:46:03 UTC 2025

--- platforms/extension/safari/filters.js
+++ platforms/extension/safari/filters.js
@@ -359,8 +359,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/safari/filters/2_optimized.txt",
 			"trustLevel": "full",
-			"version": "2.3.68.75",
-			"timeUpdated": "2025-01-21T21:31:29+0000",
+			"version": "2.3.68.76",
+			"timeUpdated": "2025-01-21T22:31:31+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.68.75",
-			"timeUpdated": "2025-01-21T21:31:29+0000",
+			"version": "2.3.68.76",
+			"timeUpdated": "2025-01-21T22:31:31+0000",
 			"languages": []
 		},
 		{
