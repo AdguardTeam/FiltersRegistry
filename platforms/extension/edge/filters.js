5e31ba20b06276cc1539926836b5f5b974704a2d
Filters Builder <filters@adguard.com>
2025-02-28 01:43:01 +0000

    skip ci. build from Fri Feb 28 01:43:00 UTC 2025

--- platforms/extension/edge/filters.js
+++ platforms/extension/edge/filters.js
@@ -1682,8 +1682,8 @@
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/edge/filters/224.txt",
 			"trustLevel": "full",
-			"version": "2.1.32.28",
-			"timeUpdated": "2025-02-27T23:38:21+0000",
+			"version": "2.1.32.29",
+			"timeUpdated": "2025-02-28T01:38:38+0000",
 			"deprecated": false,
 			"languages": [
 				"zh"
--- platforms/ios/filters.js
+++ platforms/ios/filters.js
@@ -1657,8 +1657,8 @@
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/ios/filters/224_optimized.txt",
 			"trustLevel": "full",
-			"version": "2.1.32.28",
-			"timeUpdated": "2025-02-27T23:38:21+0000",
+			"version": "2.1.32.29",
+			"timeUpdated": "2025-02-28T01:38:38+0000",
 			"deprecated": false,
 			"languages": [
 				"zh"
