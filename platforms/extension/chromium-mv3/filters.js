2f3d4f741021afc3541dfb8f9bccaf6c3cca1273
Filters Builder <filters@adguard.com>
2025-03-18 19:37:20 +0000

    skip ci. build from Tue Mar 18 19:37:19 UTC 2025

--- platforms/extension/chromium-mv3/filters.js
+++ platforms/extension/chromium-mv3/filters.js
@@ -359,8 +359,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/chromium-mv3/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.75.70",
-			"timeUpdated": "2025-03-18T16:31:33+0000",
+			"version": "2.3.75.71",
+			"timeUpdated": "2025-03-18T19:31:34+0000",
 			"deprecated": false,
 			"languages": [],
 			"tags": [
--- platforms/mac/filters.json
+++ platforms/mac/filters.json
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.75.70",
-			"timeUpdated": "2025-03-18T16:31:33+0000",
+			"version": "2.3.75.71",
+			"timeUpdated": "2025-03-18T19:31:34+0000",
 			"languages": []
 		},
 		{
